9b223d10f11ff2bb627421db260b1cc9108c2de2
oseminikhin <oseminikhin@tolkit.top>
2023-05-10 08:53:27 +0300

    erro response added

--- src/assets/responses.ts
+++ src/assets/responses.ts
@@ -3,6 +3,7 @@ export const UNREGISTERED_RESPONSE = "Ты не один из нас!";
 export const REGISTERED_RESPONSE = "Ты один из нас!";
 export const SUCCESS_REGISTERATION_RESPONSE = "Теперь ты один из нас!\n\n";
 export const REMOVE_USER_RESPONSE = "Теперь ты не один из нас!"
+export const ERROR_RESPONSE = "===Произошла ошибка==="
 
 export const HELP_RESPONSE = `
 /help : показать список доступных команд;\n\n
